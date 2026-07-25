6cdc3f8336b38c9b2e898d6690ed0080e61d3aef
Samuel Cristiano <samuelcsa.ferreira@outlook.com>
2026-07-25 19:48:09 -0300

    Changes: improve VendasService and VendasRepository

--- app/repository/SalesRepository.py
+++ app/repository/SalesRepository.py
@@ -1,12 +1,9 @@
 # =======================================
 # IMPORTAÇÃO DAS BIBLIOTECAS
 # =======================================
-# Bibliotecas padrão
-from datetime import datetime
-import json
-
 # Bibliotecas externas
-import psycopg2
+from sqlalchemy import create_engine, text
+import pandas as pd
 
 # Bibliotecas internas
 from app.settings import DATABASE_URL
@@ -21,16 +18,17 @@ class SalesRepository:
 
     Centraliza as operações de acesso ao banco de dados, incluindo
     criação da tabela e manipulação dos registros.
-
+    
     Attributes:
         connection (psycopg2.extensions.connection):
             Conexão ativa com o banco de dados PostgreSQL.
     """
 
     def __init__(self, database: str = DATABASE_URL) -> None:
-        self.connection: psycopg2.extensions.connection = (
-            psycopg2.connect(database)
-        )
+        self.engine = create_engine(database, pool_size=10, max_overflow=20)
+        # self.connection: psycopg2.extensions.connection = (
+        #     psycopg2.connect(database)
+        # )
         self._create_table()
 
 
@@ -49,95 +47,74 @@ class SalesRepository:
         - sales_dt (DATE): Data em que a venda foi realizada.
         - created_at (TIMESTAMP): Data e hora em que foi criado.
         """
+
+        # Comando SQL.
+        sql ="""
+            CREATE TABLE IF NOT EXISTS sales(
+                id SERIAL PRIMARY KEY,
+                price NUMERIC(10,2) NOT NULL,
+                pay_method VARCHAR(20),
+                sales_dt DATE,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        """
+
+        # Cria a tabela, caso ela ainda não exista.
+        with self.engine.connect() as con:
+            con.execute(text(sql))
+            con.commit()
+
+
+    def register_sale(self, price: float, pay_method: str, sales_dt) -> None:
+        """
+        Insere os dados na tabela do banco de dados.
+            Args:
+            - price (float)
+            - pay_method(str)
+            - date (str)
+        """
+
+        # Comando SQL.
+        sql = """
+            INSERT INTO sales (price, pay_method, sales_dt)
+                VALUES (:price, :pay_method, :sales_dt)"""
         
-        with self.connection as con:
-            cur = con.cursor()
-
-            # Cria a tabela caso ela ainda não exista.
-            cur.execute("""
-                CREATE TABLE IF NOT EXISTS sales(
-                    id SERIAL PRIMARY KEY,
-                    price NUMERIC(10,2) NOT NULL,
-                    pay_method VARCHAR(20),
-                    sales_dt DATE,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                );
-            """)
+        # Rregistra uma venda no banco de dados.
+        with self.engine.connect() as con:
+            con.execute(text(sql), {"price": price, "pay_method": pay_method, "sales_dt": sales_dt})
+            con.commit()
 
 
-    # Registra uma venda no bano de dados.
-    def register_sale(self, price: float, pay_method: str, date: str):
+    def read_sales(self, start_date: str = None, end_date: str = None):
+        """Executa a leitura da tabela de vendas e retorna os dados do banco em uma DataFrame (Pandas)
         """
-        Insere dados dentro do banco de dados.
 
-        **`INSERT INTO...`:**
-        - dasda
-        """
-        with self.connection as con:
-            cur = con.cursor()
-            cur.execute("""
-                INSERT INTO vendas_table (price, pay_method, sales_dt)
-                    VALUES (%s, %s, %s)""",
-                (price, pay_method, date),
-            )
-            return cur.lastrowid
+        # Comando SQL.
+        sql="""
+            SELECT price, pay_method, sales_dt 
+                FROM sales
+            """
+        
+        # Faz a leitura de todos os dados da tabela.
+        with self.engine.connect() as con:
+            sales = pd.read_sql(text(sql), con=con)
 
-    def ler_registros(self):
-        with self.connection as con:
-            cur = con.cursor()
-            cur.execute(
-                "SELECT id, valor, form_pgt, vd_date FROM vendas_table")
-            coluna = [descricao[0] for descricao in cur.description]
-            dados = cur.fetchall()
+            # Formata a coluna da data da venda para string.
+            sales['sales_dt'] = pd.to_datetime(sales['sales_dt']).dt.strftime('%d/%m/%Y')
+        
+        # Verifica se foi repassado a data de inicio e fim do periodo.
+        if start_date and end_date:
+            sql = """
+                SELECT price, pay_method, sales_dt 
+                    FROM sales
+                    WHERE sales_dt BETWEEN :start AND :end
+                """
+            # Faz a leitura dos dados dentro da data selecionada.
+            with self.engine.connect() as con:
+                sales = pd.read_sql(text(sql), params={"start": start_date, "end": end_date}, con=con)
 
-            dados_json = [dict(zip(coluna, linha)) for linha in dados]
-            # transformar coluna vd_date em string
-            dados_json = [
-                {**linha, "vd_date": linha["vd_date"].strftime("%d-%m-%y")}
-                for linha in dados_json
-            ]
+                # Formata a coluna da data da venda para string.
+                sales['sales_dt'] = pd.to_datetime(sales['sales_dt']).dt.strftime('%d/%m/%Y')
+                
 
-            with open("dados_json.json", "w", encoding="utf-8") as arquivo_json:
-                json.dump(dados_json, arquivo_json,
-                          indent=4, ensure_ascii=False)
-            return dados_json
-
-    def selecioar_dados_periodo(self, data_inicio, data_final):
-        with self.connection as con:
-            cur = con.cursor()
-            cur.execute(
-                "SELECT id, valor, form_pgt, vd_date FROM vendas_table WHERE vd_date BETWEEN %s AND %s",
-                (data_inicio, data_final),
-            )
-
-            dados = cur.fetchall()
-            return dados
-
-    def atualizar_venda(self, id, valor, pagamento, data):
-        with self.connection as con:
-            cur = con.cursor()
-            cur.execute(
-                "UPDATE vendas_table SET valor = %s, form_pgt = %s, vd_date = %s WHERE id = %s",
-                (valor, pagamento, data, id),
-            )
-            return cur.rowcount
-
-    def deletar_registro(self, venda_id):
-        with self.connection as con:
-            cur = con.cursor()
-            cur.execute(
-                "DELETE FROM vendas_table WHERE id = %s",
-                (venda_id,))
-            return cur.rowcount
-
-
-    def somar_vendas(self, forma_pagamento):
-        with self.connection as con:
-            cur = con.cursor()
-            cur.execute(
-                "SELECT SUM(valor) FROM vendas_table WHERE form_pgt = %s",
-                (forma_pagamento,),
-            )
-
-            dados = cur.fetchone()
-            return dados
+        return sales
--- app/services/vendasService.py
+++ app/services/vendasService.py
@@ -3,31 +3,68 @@ from app.repository.SalesRepository import SalesRepository
 
 
 class VendasService:
-    """Classe de Serviço para vendas. Responsável por instanciar
-    a classe Vendas e Registrar suas informações no banco através
-    da clase de SalesRepository"""
+    """Serviço da classe vendas.
+    
+    Esse serviço é reponsável por executar ações e validações do usuário,
+    através da comunicação entre o repositorio e a classe Vendas.
+    """
 
     def __init__(self, repository=None):
-        self.dados = None
         self.repository = repository or SalesRepository()
 
-    def registrar_venda(self, valor, pagamento, data):
-        registro = Vendas(valor, pagamento, data)
-        self.repository.criar_venda(registro.valor, registro.pagamento, registro.data)
-        return registro
 
-    def mostrar_registros(self, data_inicio=None, data_final=None):
-        self.dados = self.repository.ler_registros()
-        return self.dados
+    def create_sales(self, price, pay_method, sales_dt):
+        """
+        Coleta os dados passados e cria uma instancia de vendas
+        a ser registrado no repositorio.
 
-    def deletar_dado(self, venda_id):
-        self.id = venda_id
-        self.repository.deletar_registro(self.id)
-        return "Dado deletado"
+        Args:
+            price (float):
+                Valor da venda.
+            pay_method (string):
+              Forma de pagamento registrada.
+            sales_dt (string):
+                Data em que foi realizada a venda.
+        """
 
-    def exibir_soma(self, forma_pagamento):
-        self.forma_pagamento = self.repository.somar_vendas(forma_pagamento)
-        soma = self.forma_pagamento[0]
-        if soma is None:
-            soma = float(0)
-        return soma
+        sales = Vendas(price, pay_method, sales_dt)
+        self.repository.register_sale(sales.price, sales.pay_method, sales.sales_dt)
+        return sales
+
+
+    def get_sales(self, start_dt=None, end_dt=None):
+        """
+        Coleta todos os dados da tabela sales.
+
+
+        Retorna todas os dados do perido caso seja informado algum argumento.
+        Args:
+            start_dt (str):
+                Data inicial da busca.
+            end_dt (str):
+                Data final da busca
+            
+
+        Permite coletar as vendas do banco, sendo possivel a coleta por período
+        através dos argumentos .
+
+        Caso os argumentos
+        """
+
+        # Coleta todos as vendas do repositorio sales.
+        self.dados = self.repository.read_sales()
+
+        # Verifica se a data de inicio e fim foram preenchidas.
+        if start_dt and end_dt:
+            # Coleta todas as vendas entre as datas selecionadas.
+            self.dados = self.repository.read_sales(start_dt, end_dt)
+            
+
+        # Soma as vendas por forma de pagamento.
+        self.resumo = self.dados.groupby(self.dados['pay_method'])['price'].sum().reset_index()
+
+        self.dados = self.dados.to_dict(orient='records')
+
+        self.resumo = self.resumo.to_dict(orient='records')
+
+        return self.dados, self.resumo
